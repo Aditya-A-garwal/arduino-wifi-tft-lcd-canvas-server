4de7f662689164e42d0defd44fb1318ba2ebb7fc
Aditya Agarwal <aditya.agarwal030504@gmail.com>
2024-02-12 18:44:15 +0530

    feat: add code
    Signed-off-by: Aditya Agarwal <aditya.agarwal030504@gmail.com>

---
+++ src/image.rs
@@ -0,0 +1,152 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{Seek, SeekFrom};
+
+use byteorder::*;
+
+pub fn save_bmp_image(data: &[Vec<u16>], filename: &str) {
+
+    let height = data.len();
+    let width = data.first().unwrap().len();
+
+    // Calculate the size of the image data in bytes
+    let row_size = width * 2; // Each pixel is 16 bits (2 bytes)
+    let padding_size = (4 - (row_size % 4)) % 4; // Calculate padding needed per row
+    let image_size = (row_size + padding_size) * height;
+
+    let mut bmp_header = Vec::with_capacity(14);
+    let mut dib_header = Vec::with_capacity(40);
+
+    let padding = vec![0; padding_size];
+
+    bmp_header.write_all(b"BM").unwrap(); // Write the 2-byte string "BM"
+    bmp_header
+        .write_u32::<LE>(54 + (image_size as u32))
+        .unwrap(); // Write a 32-bit unsigned integer (image size + 54)
+    bmp_header.write_u16::<LE>(0).unwrap(); // Write a 16-bit unsigned integer (0)
+    bmp_header.write_u16::<LE>(0).unwrap(); // Write a 16-bit unsigned integer (0)
+    bmp_header.write_u32::<LE>(54).unwrap(); // Write a 32-bit unsigned integer (54)
+
+    dib_header.write_u32::<LE>(40).unwrap(); // Write a 32-bit unsigned integer (40)
+    dib_header.write_i32::<LE>(width as i32).unwrap(); // Write a 32-bit signed integer (width)
+    dib_header.write_i32::<LE>(height as i32).unwrap(); // Write a 32-bit signed integer (height)
+    dib_header.write_u16::<LE>(1).unwrap(); // Write a 16-bit unsigned integer (1)
+    dib_header.write_u16::<LE>(16).unwrap(); // Write a 16-bit unsigned integer (16)
+    dib_header.write_u32::<LE>(0).unwrap(); // Write a 32-bit unsigned integer (0)
+    dib_header.write_u32::<LE>(image_size as u32).unwrap(); // Write a 32-bit unsigned integer (image size)
+    dib_header.write_u32::<LE>(0).unwrap(); // Write a 32-bit unsigned integer (0)
+    dib_header.write_u32::<LE>(0).unwrap(); // Write a 32-bit unsigned integer (0)
+    dib_header.write_u32::<LE>(0).unwrap(); // Write a 32-bit unsigned integer (0)
+    dib_header.write_u32::<LE>(0).unwrap(); // Write a 32-bit unsigned integer (0)
+
+    // Write to BMP file
+    let mut bmp_file =
+        File::create(format!("{}.bmp", filename)).expect("Failed to create BMP file");
+    bmp_file
+        .write_all(&bmp_header)
+        .expect("Failed to write BMP header");
+    bmp_file
+        .write_all(&dib_header)
+        .expect("Failed to write DIB header");
+
+    // Write pixel data
+    for row in data.iter().rev() {
+        for &v in row {
+            bmp_file
+                .write_all(&v.to_le_bytes())
+                .expect("Failed to write pixel data");
+        }
+
+        // Write padding bytes
+        bmp_file
+            .write_all(&padding)
+            .expect("Failed to write padding");
+    }
+}
+
+pub fn load_bmp_image(filename: &str) -> Vec<Vec<u16>> {
+    // Open the BMP file
+    let mut bmp_file =
+        File::open(format!("{}.bmp", filename)).expect("Failed to open BMP file");
+
+    // Read the BMP header
+    let mut bmp_header = [0; 54];
+    bmp_file
+        .read_exact(&mut bmp_header)
+        .expect("Failed to read BMP header");
+
+    // Move to the start of pixel data
+    bmp_file
+        .seek(SeekFrom::Start(54))
+        .expect("Failed to seek to pixel data");
+
+    // Extract image width and height from the header
+    let width = u32::from_le_bytes([
+        bmp_header[18],
+        bmp_header[19],
+        bmp_header[20],
+        bmp_header[21],
+    ]) as usize;
+    let height = u32::from_le_bytes([
+        bmp_header[22],
+        bmp_header[23],
+        bmp_header[24],
+        bmp_header[25],
+    ]) as usize;
+
+    // Calculate the size of each row, including padding if necessary
+    let row_size = width * 2; // Each pixel is 16 bits (2 bytes)
+    let padding_size = (4 - (row_size % 4)) % 4; // Calculate padding needed per row
+
+    let mut padding = vec![0; padding_size];
+
+    // Read the pixel data
+    let mut pixels = vec![vec![0; width]; height];
+    let mut color_data = [0, 0];
+
+    for row in pixels.iter_mut().rev() {
+        for element in row.iter_mut() {
+            bmp_file
+                .read_exact(&mut color_data)
+                .expect("Failed to read color data");
+
+            *element = u16::from_le_bytes([color_data[0], color_data[1]]);
+        }
+
+        bmp_file
+            .read_exact(&mut padding)
+            .expect("Failed to read padding data");
+    }
+
+    pixels
+}
+
+pub fn color_2_code(clr: u16) -> Result<u8, ()> {
+    match clr {
+        0xF800u16 => Ok(0),
+        0x07E0u16 => Ok(1),
+        0x001Fu16 => Ok(2),
+        0x07FFu16 => Ok(3),
+        0xF81Fu16 => Ok(4),
+        0xFFE0u16 => Ok(5),
+        0xFFFFu16 => Ok(6),
+        0x520Au16 => Ok(7),
+        0x0000u16 => Ok(8),
+        _ => Err(()),
+    }
+}
+
+pub fn code_2_color(code: u8) -> Result<u16, ()> {
+    match code {
+        0 => Ok(0xF800u16),
+        1 => Ok(0x07E0u16),
+        2 => Ok(0x001Fu16),
+        3 => Ok(0x07FFu16),
+        4 => Ok(0xF81Fu16),
+        5 => Ok(0xFFE0u16),
+        6 => Ok(0xFFFFu16),
+        7 => Ok(0x520Au16),
+        8 => Ok(0x0000u16),
+        _ => Err(()),
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,206 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+use pbr::ProgressBar;
+
+use clap::Parser;
+
+use crate::image::*;
+
+mod image;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+
+    /// Port on which to list for incoming requests
+    #[arg(short, long, default_value_t = 5005)]
+    port: u16,
+
+    /// Path to directory where images are stored
+    #[arg(short, long, default_value_t = String::from("images-dir"))]
+    image_dir: String
+}
+
+fn handle_client(mut stream: TcpStream, dir: &str) {
+    let loading_bar_width = 96;
+
+    let Ok(peer) = stream.peer_addr() else {
+        println!("Failed to read peer");
+        return;
+    };
+
+    // get the first give bytes, which are the image ID and dimensions
+    let mut buffer = [0; 6];
+    let Ok(_) = stream.read_exact(&mut buffer) else {
+        println!("Failed Request");
+        return;
+    };
+
+    let rw = buffer[0];
+    let name = buffer[1];
+    let height = (buffer[2] as usize) + ((buffer[3] as usize) << 8);
+    let width = (buffer[4] as usize) + ((buffer[5] as usize) << 8);
+
+    if rw == 1 {
+        println!(
+            r#"
+            Receiving new image from "{peer}" with
+            Dimensions: {height} x {width}
+            name: image_{name}.bmp
+            "#
+        );
+
+        let mut img = Vec::with_capacity(height);
+
+        let mut pb = ProgressBar::new(height as u64);
+        pb.set_width(Some(loading_bar_width));
+
+        for row in 0..height {
+
+            let mut mode = [0u8];
+            let mut codes = vec![0; width];
+
+            let Ok(_) = stream.read_exact(&mut mode) else {
+                println!("Error reading mode");
+                return;
+            };
+
+            if mode[0] == 0 { // normal mode
+
+                let Ok(_) = stream.read_exact(&mut codes) else {
+                    println!("Error reading row {row}");
+                    return;
+                };
+            }
+            else { // compressed mode
+
+                let mut segments_bytes = vec![0u8; 2 * (mode[0] as usize)];
+                let mut segments = vec![0u16; mode[0] as usize];
+
+                let Ok(_) = stream.read_exact(&mut segments_bytes) else {
+                    println!("Error reading compressed row {row}");
+                    return;
+                };
+
+                for (i, &e) in segments_bytes.iter().enumerate() {
+
+                    if i % 2 == 1 {
+                        segments[i / 2] |= (e as u16) << 8;
+                    }
+                    else {
+                        segments[i / 2] = e as u16;
+                    }
+                }
+
+                let mut idx = 0;
+                for &segment in segments.iter() {
+
+                    let code = (segment & 0xF) as u8;
+                    let count = (segment >> 4) & 0x1FF;
+
+                    for _ in 0..count {
+                        codes[idx] = code;
+                        idx += 1;
+                    }
+                }
+            }
+            img.push(codes.iter().map(|&v| code_2_color(v).unwrap()).collect());
+
+            pb.inc();
+        }
+        pb.finish();
+        println!();
+
+        save_bmp_image(&img, &format!("{dir}/image_{name}"));
+    } else if rw == 2 {
+        println!(
+            r#"
+            Sending new image from "{peer}" with
+            Dimensions: {height} x {width}
+            name: image_{name}.bmp
+            "#
+        );
+
+        let img = load_bmp_image(&format!("{dir}/image_{name}"));
+
+        let mut pb = ProgressBar::new(height as u64);
+        pb.set_width(Some(loading_bar_width));
+
+        for (i, row) in img.iter().enumerate() {
+            let val: Vec<u8> = (*row).iter().map(|&v| color_2_code(v).unwrap()).collect();
+
+            let Ok(_) = stream.write_all(&val) else {
+                println!("Not able to send row {i}");
+                return;
+            };
+
+            pb.inc();
+        }
+        pb.finish_println("");
+    }
+}
+
+fn main() {
+
+    let args = Args::parse();
+
+    // Define the host and port to listen on
+    let host = "0.0.0.0";
+    let port = args.port;
+
+    // Name of images directory
+    let image_dir = args.image_dir;
+
+    // create the folder where images are stored
+    match std::fs::create_dir(&image_dir) {
+
+        Ok(()) => println!("Successfully created images directory"),
+        Err(err) => {
+            if err.kind() == std::io::ErrorKind::AlreadyExists {
+                println!("Found image directory")
+            }
+            else {
+                panic!("Failed to create image directory");
+            }
+        }
+    };
+
+    // Bind to the host and port
+    let listener = match TcpListener::bind((host, port)) {
+        Ok(listener) => listener,
+        Err(err) => {
+            if err.kind() == std::io::ErrorKind::PermissionDenied {
+                println!("Permission denied while binding server to port {port}");
+                println!("hint: use sudo on linux");
+            }
+            else {
+                println!("Failed to bind server to port {port}");
+            }
+            return;
+        }
+    };
+    // let listener = TcpListener::bind((host, port)).expect("Failed to bind");
+
+    // println!("TCP server is listening on {}:{}", host, port);
+    println!("Waiting for requests on port {}", port);
+
+    // Accept incoming connections
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+
+                let dir = image_dir.clone();
+
+                // Spawn a new thread to handle each client connection
+                thread::spawn(move || {
+                    handle_client(stream, &dir);
+                });
+            }
+            Err(e) => {
+                eprintln!("Failed to accept connection: {}", e);
+            }
+        }
+    }
+}
