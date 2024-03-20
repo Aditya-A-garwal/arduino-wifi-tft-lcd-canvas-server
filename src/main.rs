fb93429386e3942ba9767ffaf7ebebde66454253
Aditya Agarwal <aditya.agarwal030504@gmail.com>
2024-03-20 16:24:42 +0530

    docs: add crate-level docs
    Signed-off-by: Aditya Agarwal <aditya.agarwal030504@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,15 @@
 #![doc(html_favicon_url = "https://i0.wp.com/dumblebots.com/wp-content/uploads/2023/12/dumblebots-logo-round.png")]
 #![doc(html_logo_url = "https://i0.wp.com/dumblebots.com/wp-content/uploads/2023/12/dumblebots-logo-round.png")]
 
+//! ![GitHub License](https://img.shields.io/github/license/Aditya-A-garwal/Arduino-WiFi-TFT-LCD-Canvas-Server)
+//! ![GitHub forks](https://img.shields.io/github/forks/Aditya-A-garwal/Arduino-WiFi-TFT-LCD-Canvas-Server?style=flat-square&color=blue)
+//! ![GitHub Repo stars](https://img.shields.io/github/stars/Aditya-A-garwal/Arduino-WiFi-TFT-LCD-Canvas-Server?style=flat-square&color=blue)
+//! ![GitHub issues](https://img.shields.io/github/issues-raw/Aditya-A-garwal/Arduino-WiFi-TFT-LCD-Canvas-Server?style=flat-square&color=indianred)
+//! ![GitHub closed issues](https://img.shields.io/github/issues-closed-raw/Aditya-A-garwal/Arduino-WiFi-TFT-LCD-Canvas-Server?style=flat-square)
+//! ![GitHub pull requests](https://img.shields.io/github/issues-pr/Aditya-A-garwal/Arduino-WiFi-TFT-LCD-Canvas-Server?style=flat-square&color=indianred)
+//! ![GitHub closed pull requests](https://img.shields.io/github/issues-pr-closed/Aditya-A-garwal/Arduino-WiFi-TFT-LCD-Canvas-Server?style=flat-square)
+//! ![GitHub Actions Workflow Status](https://img.shields.io/github/actions/workflow/status/Aditya-A-garwal/Arduino-WiFi-TFT-LCD-Canvas-Server/build.yml?style=flat-square)
+//!
 //! # Arduino WiFI TFT LCD Canvas Server
 //! Server for the [Arduino WiFi TFT LCD Canvas App](https://github.com/Aditya-A-garwal/Arduino-WiFi-TFT-LCD-Canvas-App).
 
